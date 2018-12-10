4ac82846b50fb6f7c6ad376ba539555fdc4bc0d5
Mario Barone <mario.e.barone@gmail.com>
2018-12-10 21:11:09 +1100

    Initial commit

---
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialogue : MonoBehaviour {
+
+    [SerializeField] private GameObject continueButton;
+    [SerializeField] private TextMeshProUGUI textDisplay;
+    [SerializeField] private Animator textDisplayAnim;
+
+    /// <summary>
+    /// The array of sentences that will be displayed, currently does not handle user stories, strictly hard coded sentences
+    /// </summary>
+    [SerializeField] private string[] sentences;
+    /// <summary>
+    /// The Speed at which letters will be shown
+    /// </summary>
+    [SerializeField] private float typingSpeed;
+
+
+
+    private int index;
+
+
+    private void Start()
+    {
+
+        StartCoroutine(Type());
+    }
+
+    private void Update()
+    {
+        if(textDisplay.text == sentences[index])
+        {
+            continueButton.SetActive(true);
+        }
+    }
+
+    IEnumerator Type()
+    {
+        foreach (char letter in sentences[index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    public void NextSentence()
+    {
+
+        textDisplayAnim.SetTrigger("change");
+        continueButton.SetActive(false);
+
+        if (index < sentences.Length - 1)
+        {
+            index++;
+            textDisplay.text = "";
+            StartCoroutine(Type());
+        }
+        else
+        {
+            textDisplay.text = "";
+            continueButton.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
+    private Vector2 moveInput;
+    private Vector2 moveVelocity;
+
+    [SerializeField] private float baseMovementSpeed = 1f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        sr.flipX = rb.transform.localPosition.x < 0  ? true : false;
+
+
+
+        moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput.normalized * baseMovementSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
+}
