21febec70bffe0bb3853a7d7e23febf1c9be96e0
Mario Barone <mario.e.barone@gmail.com>
2018-12-12 19:37:40 +1100

    player?

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 [RequireComponent(typeof(PlayerSprites))]
 [RequireComponent(typeof(Controller2D))]
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
     [SerializeField] private float moveSpeed = 6f;
 
     private Controller2D controller;
     private CameraController cameraController;
     private PlayerSprites sprites;
+    private Animator playerAnimator;
 
     private Vector2 velocity;
 
     private void Awake()
     {
+        playerAnimator = GetComponent<Animator>();
         controller = GetComponent<Controller2D>();
         cameraController = Camera.main.GetComponent<CameraController>();
         sprites = GetComponent<PlayerSprites>();
@@ -26,5 +29,13 @@ public class Player : MonoBehaviour {
         velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         velocity = Vector2.ClampMagnitude(velocity, 1);
         controller.Move(velocity * moveSpeed * Time.deltaTime);
+
+        bool result = velocity > 0 ? true : false;
+    }
+
+    private bool HandleAnims(float v)
+    {
+       bool result = v > 0 ?  true :  false;
+
     }
 }
