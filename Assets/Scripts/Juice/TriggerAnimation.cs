1523e2d5d0621727382031b6ba95d17ba97d676d
Mario Barone <mario.e.barone@gmail.com>
2018-12-13 23:22:21 +1100

    puff, idle, heart container, trigger anim (grass anim) impact placeholder

---
+++ Assets/Scripts/Juice/TriggerAnimation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerAnimation : MonoBehaviour
+{
+
+    [SerializeField] private string animTriggerName;
+    [SerializeField] private string triggererTag;
+    private Animator objAnim;
+
+    private void Start()
+    {
+        objAnim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //if (other.CompareTag(triggererTag))
+        //{
+            objAnim.SetTrigger(animTriggerName);
+        //}
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //if (collision.gameObject.CompareTag(triggererTag))
+        //{
+            objAnim.SetTrigger(animTriggerName);
+        //}
+    }
+}
